fb25438551843a989f1dc7eb47d1ef44fa103f89
Pavel Abramov <pavel.abramov.spb@gmail.com>
2018-01-17 14:59:54 +0300

    Updates

---
+++ assets/OPTIONS.ts
@@ -0,0 +1,29 @@
+export const OPTIONS = [{
+			name: 'Slider-1',
+			count: 2,
+			items: [{
+				name: 'first',
+				count: 3,
+				percent: 0
+			}, {
+				name: 'second',
+				count: 1,
+				percent: 0
+			}]
+		}, {
+			name: 'Slider-2',
+			count: 3,
+			items: [{
+				name: 'first',
+				count: 3,
+				percent: 0
+			}, {
+				name: 'second',
+				count: 1,
+				percent: 0
+			}, {
+				name: 'third',
+				count: 1,
+				percent: 0
+			}]
+		}]
